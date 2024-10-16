ee2d88b070e7a52f29a1d587d56ff966beeddd66
rayhansifat <sifatrayhan140@gmail.com>
2024-10-16 11:25:41 +0600

    adding main section cards and heading

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,37 @@
+import { assets } from '../../assets/assets'
 import './Main.css'
 
 export const Main = () => {
   return (
-    <div>Main</div>
+    <div className='main'>
+        <div className="nav">
+            <p>Gemini</p>
+            <img src={assets.user_icon} alt="" />
+        </div>
+        <div className="main-container">
+            <div className="greet">
+                <p><span>Hello, Dev.</span></p>
+                <p>How can i help you today?</p>
+            </div>
+            <div className="cards">
+                <div className="card">
+                    <p>Suggest beautiful places to see on an becoming road trip</p>
+                    <img src={assets.compass_icon} alt="" />
+                </div>
+                <div className="card">
+                    <p>Briefly summarize this concept: urban planning</p>
+                    <img src={assets.bulb_icon} alt="" />
+                </div>
+                <div className="card">
+                    <p>Brainstrom team bonding activities for our work retreat</p>
+                    <img src={assets.message_icon} alt="" />
+                </div>
+                <div className="card">
+                    <p>Suggest beautiful places to see on an becoming road trip</p>
+                    <img src={assets.code_icon} alt="" />
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
